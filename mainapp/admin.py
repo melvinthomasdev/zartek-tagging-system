e11d5c8c93da3a8f7c28085c08c953517b102ff7
Melvin Thomas <melvichooranolil@gmail.com>
2022-04-13 22:49:45 +0530

    Models OK

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,8 +1,30 @@
 from atexit import register
+from pyexpat import model
+from statistics import mode
 from django.contrib import admin
 
-from mainapp.models import Profile
+from mainapp.models import Profile, Post, PostImage, Tag, PostTag
 
-# Register your models here.
 
+class PostImageAdmin(admin.StackedInline):
+    model = PostImage
+
+
+class TagAdmin(admin.ModelAdmin):
+
+    class Meta:
+        model = Tag
+
+class PostTagAdmin(admin.StackedInline):
+    model = PostTag
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [PostImageAdmin, PostTagAdmin ]
+
+    class Meta:
+        model = Post
+
+admin.site.register(Post,PostAdmin)
 admin.site.register(Profile)
+admin.site.register(Tag)
--- mainapp/models.py
+++ mainapp/models.py
@@ -1,5 +1,6 @@
 from email.policy import default
 from hashlib import blake2b
+from pyexpat import model
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -12,3 +13,31 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+class PostImage(models.Model):
+    post = models.ForeignKey(Post, related_name='postimage', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='PostImages')
+    
+
+class Tag(models.Model):
+    text = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.text
+
+
+class PostTag(models.Model):
+    post = models.ForeignKey(Post, related_name='posttag', on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    weight = models.IntegerField()
+
+    def __str__(self):
+        return self.tag.text
